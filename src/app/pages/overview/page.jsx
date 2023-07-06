316aff951b7dc1b93929d4929033f63d1e40e0e5
raohassansoc <129951994+raohassansoc@users.noreply.github.com>
2023-07-06 19:23:03 +0500

    fedf'

--- src/app/pages/overview/page.jsx
+++ src/app/pages/overview/page.jsx
@@ -2,25 +2,17 @@
 import Footer from "@/app/components/common/Footer";
 import NavBar from "@/app/components/common/Navbar";
 import OverviewMain from "@/app/components/overview/OverviewMain";
-import React, {useState} from "react";
 
-const page = () => {
-  const [isload, isLoadSet] = useState(true);
+const Page = () => {
   return (
-    <div>
-      {isload ? (
-        <div className="bg-black">
-          <NavBar />
-          <div className="bg-[#232323] p-1 box-border"></div>
-          <OverviewMain />
-          <div className="bg-[#232323] p-1 box-border"></div>
-          <Footer />
-        </div>
-      ) : (
-        <div class="loader"></div>
-      )}
+    <div className="bg-black">
+      <NavBar />
+      <div className="bg-[#232323] p-1 box-border"></div>
+      <OverviewMain />
+      <div className="bg-[#232323] p-1 box-border"></div>
+      <Footer />
     </div>
   );
 };
 
-export default page;
+export default Page;
